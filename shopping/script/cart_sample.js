afa71d0e79278e25817b3c9e3042a03d8f525c57
mina <viva3927@google.com>
2024-01-08 17:01:36 +0900

    오늘의 집 상세페이지 완성

--- shopping/script/cart_sample.js
+++ shopping/script/cart_sample.js
@@ -80,8 +80,104 @@ console.log(delivery_menu, delivery_menu_open, delivery_menu_btn);
 delivery_menu_open.style.display = 'none';
 
 delivery_menu_btn.addEventListener('click',function(){
-    delivery_menu_open.style.display = 'block';
-    delivery_menu_btn.style.transform = 'scale(-1)';
+    delivery_menu_open.style.display = 'flex'; //기존에 flex로 디자인을 했기 때문에!
+    delivery_menu_btn.style.transform = 'scaleY(-1)';
     delivery_menu.style.borderBottomLeftRadius = '0';
     delivery_menu.style.borderBottomRightRadius = '0';
+    // delivery_menu.style = 'border-bottom-left-radius:0; border-bottom-right-radius:0;' //이렇게 해도 됨
+})
+
+//상품 색상, 사이즈 옵션을 선택했을 때 선택 정보가 selectResult에 결과값으로 출력되고 num_result의 구매수량에 따라 order_price에 가격이 출력되는 결과
+/* 상품 색상을 선택하지 않으면 사이즈 옵션을 선택 할 수 없음
+색상별로 사이즈 옵션이 다르기 때문에 색상별로 사이즈가 묶여있음
+상품 색상을 선택하고 사이즈 옵션이 보여서 사이즈를 선택하면 아래에 내가 선택한 옵션이 아래에 뜸
+동일한 색상과 사이즈를 누르면 중복되어서 '이미 선택한 옵션입니다.' 팝업창이 뜸
+수량을 -+로 조절할 수 있음
+수량 숫자를 선택하면 수량을 입력해서 적용할 수 있음
+X버튼을 누르면 내가 선택한 옵션이 삭제되고 다시 그 부분에 장바구니/바로구매 버튼이 올라옴
+옵션을 선택하지 않고 장바구니/바로구매 버튼을 누르면 '옵션 선택 후에 버튼을 클릭해 주세요.' 팝업창이 뜬다. */
+//--------------------------------------------------------
+//상세절차 : 상품 색상, 사이즈 옵션을 선택했을 때
+//1. 색상(옵션1) 선택 전 사이즈(옵션2) 비활성화
+//2. 옵션1 선택시 옵션2 활성화
+//2-1. 옵션1에 대한 option데이터에 따라 옵션2의 각 다른 select 활성화
+//3. 옵션1 선택 후 옵션2 클릭 시 결과 출력
+//3-1. (위) 조건 달성 기준 결과 출력이 되어있는 상태라면 같은 옵션 클릭시 중복 데이터 결과 팝업 출력
+//3-2. (위) 조건 달성 기준 결과 출력과 다른 옵션을 클릭 시 추가 데이터 기존 데이터 (위로) 출력
+//3-3. (위) 조건 달성 기준 옵션1, 옵션2의 선택 데이터는 초기화됨.
+const colorOpt = document.querySelector('#colorOpt')
+const sizeOpt = document.querySelector('#sizeOpt')
+const selectResult = document.querySelector('.selectResult')
+console.log(colorOpt,sizeOpt);
+console.log(colorOpt.options[1].value);
+console.log(colorOpt.options[1].value.text);
+selectResult.style.display = 'none';
+//colorOpt, sizeOpt text데이터를 모두 변수로 수집 후
+//createElement, appendChild를 이용해서 opt1, opt2 선택 데이터 출력하기
+const optResult1 = document.createElement('em')
+const optResult2 = document.createElement('em')
+const resultView = document.querySelectorAll('.selectResult .opt_list span')
+console.log(resultView);
+
+//수량 출력하기
+const numView = selectResult.querySelector('#num_count');
+const priceView = selectResult.querySelector('.order_price');
+const priceTotalView = document.querySelector('fieldset:nth-child(2) .order_price');
+let num = 1;
+let price = 36900;
+console.log(num, price, priceTotalView);
+
+colorOpt.addEventListener('change',function(){
+    console.log(colorOpt.value)
+    console.log(colorOpt.options[colorOpt.selectedIndex].text)
+    optResult1.innerHTML = colorOpt.options[colorOpt.selectedIndex].text
+    console.log(optResult1);
+})
+sizeOpt.addEventListener('change',function(){
+    //선택 option 데이터 저장하기
+    console.log(sizeOpt.options[sizeOpt.selectedIndex].text)
+    optResult2.innerHTML = sizeOpt.options[sizeOpt.selectedIndex].text
+    console.log(optResult2);
+    //선택옵션 부모 보이기
+    selectResult.style.display = 'grid';
+    //선택옵션 적용 대상에 위 option데이터값 출력하기
+    resultView[0].appendChild(optResult1)
+    resultView[1].appendChild(optResult2)
+    //선택옵션의 수량(num) 출력하기
+    numView.value = num;
+    //선택옵션의 가격(price) 출력하기
+    priceView.innerHTML = price.toLocaleString('ko-kr')+'원';
+    priceTotalView.innerHTML = price.toLocaleString('ko-kr')+'원';
+})
+
+//selectResult 안 X 클릭 시 X의 부모(selectResult)를 DOM관계로 선택해서 숨기기
+const close = selectResult.querySelector('.close');
+console.log(close);
+
+close.addEventListener('click',function(){
+    close.parentElement.style.display = 'none';
+})
+//수량 -,+ 버튼 클릭 시 수량값이 변경되며 그에 따라 가격 변동
+const minus = selectResult.querySelector('#minus');
+const plus = selectResult.querySelector('#plus');
+let total = 0;
+
+plus.addEventListener('click',function(){
+    //1. 수량 1증가
+    num += 1;
+    //1-1. 수량 1 증가한 값 표시
+    numView.value = num;
+    //2. 수량*가격 = 구매가격
+    total = num*price;
+    //3. 구매가 세자리 콤마 표시
+    priceView.innerHTML = total.toLocaleString('ko-kr')+'원'
+    priceTotalView.innerHTML = total.toLocaleString('ko-kr')+'원'
+})
+
+minus.addEventListener('click',function(){
+    num -= 1;
+    numView.value = num;
+    total = num*price;
+    priceView.innerHTML = total.toLocaleString('ko-kr')+'원';
+    priceTotalView.innerHTML = total.toLocaleString('ko-kr')+'원';
 })
