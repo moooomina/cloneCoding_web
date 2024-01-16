8c58be8448c47b85eb2a0b53e86d51ef785f691d
mina <viva3927@google.com>
2024-01-16 16:56:28 +0900

    로그인 상태 디자인

--- naver/script/login.js
+++ naver/script/login.js
@@ -14,21 +14,29 @@ console.log(id_login_container)
 console.log(disposable_login_container)
 console.log(qr_login_container)
 
-// 변수 생성 후 초기모습 설정해놓기(일회용, QR 내용 숨기기)
+//변수 생성 후 초기모습 설정해놓기(일회용, QR 내용 숨기기)
 disposable_login_container.style.display = 'none'
 qr_login_container.style.display = 'none'
+//초기값 ID로그인 활성화 시키기(active) *클릭 전
+login_title[0].parentElement.classList.add('active')
 
 //ID로그인 클릭하면 로그인내용O, 일회용내용X, QR코드내용X
 login_title[0].addEventListener('click',()=>{
     id_login_container.style.display = 'block'
     disposable_login_container.style.display = 'none'
     qr_login_container.style.display = 'none'
+    login_title[0].parentElement.classList.add('active')
+    login_title[1].parentElement.classList.remove('active')
+    login_title[2].parentElement.classList.remove('active')
 })
 //일회용 클릭하면 로그인내용X, 일회용내용O, QR코드내용X
 login_title[1].addEventListener('click',()=>{
     id_login_container.style.display = 'none'
     disposable_login_container.style.display = 'block'
     qr_login_container.style.display = 'none'
+    login_title[0].parentElement.classList.remove('active')
+    login_title[1].parentElement.classList.add('active')
+    login_title[2].parentElement.classList.remove('active')
 })
 
 //QR코드 클릭하면 로그인내용X, 일회용내용X, QR코드내용O
@@ -36,4 +44,7 @@ login_title[2].addEventListener('click',()=>{
     id_login_container.style.display = 'none'
     disposable_login_container.style.display = 'none'
     qr_login_container.style.display = 'block'
+    login_title[0].parentElement.classList.remove('active')
+    login_title[1].parentElement.classList.remove('active')
+    login_title[2].parentElement.classList.add('active')
 })
