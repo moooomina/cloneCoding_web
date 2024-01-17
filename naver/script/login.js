314e3d0b6bae9b634c99376913c9aef6e48c8208
mina <viva3927@google.com>
2024-01-17 11:36:33 +0900

    naver 로그인

--- naver/script/login.js
+++ naver/script/login.js
@@ -48,3 +48,54 @@ login_title[2].addEventListener('click',()=>{
     login_title[1].parentElement.classList.remove('active')
     login_title[2].parentElement.classList.add('active')
 })
+
+//=========================================================
+console.log('로그인 메세지 출력============================')
+//아이디 로그인 시 틀린 정보 또는 입력되지 않은 정보에 따라 error message 출력하기
+//2. 아이디 미입력 후 로그인 버튼 클릭 => "아이디를 입력해주세요"
+//2. 아이디 입력, 비밀번호 미입력 후 로그인 버튼 클릭 => "비밀번호를 입력해주세요"
+//3. 아이디, 비밀번호 입력 후 로그인 버튼 클릭 시 해당 정보가 맞지 않다면 => "아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요."
+//4. 아이디, 비밀번호 입력 후 로그인 버튼 클릭 시 해당 정보가 맞다면 => 로그인 성공 팝업 "어서오세요 OO님"
+
+const userIdInput = document.querySelector('#userId');
+const userPwInput = document.querySelector('#userPw');
+const error_message = document.querySelector('.error_message');
+const loginBtn = document.querySelector('#loginBtn');
+const userList = [{
+    id:'admin',
+    pw:'1234'
+}]
+console.log(userIdInput,userPwInput,error_message,loginBtn);
+
+console.log(userIdInput.value)
+console.log(userIdInput.value == '')
+
+loginBtn.addEventListener('click',()=>{
+    if(userIdInput.value == ''){//아이디 빈 문자열 검사 -> 참이라면
+        error_message.innerHTML = '아이디를 입력해주세요' //참일 때 실행
+    }else if(userPwInput.value == ''){
+        error_message.innerHTML = '비밀번호를 입력해주세요'
+    }else if(userIdInput.value == userList[0].id && userPwInput.value == userList[0].pw){
+        alert(`어서오세요 ${userList[0].id}님`)
+    }else{
+        error_message.innerHTML = '아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다.<br>입력하신 내용을 다시 확인해주세요.'
+        userPwInput.value = ''
+    }
+})
+
+//일회용 로그인 error_message
+const disposable_loginBtn = document.querySelector('#disposable_loginBtn')
+const userNum = document.querySelector('#userNum')
+const disposable_loginBtn_error = document.querySelector('.disposable_login .error_message')
+console.log(disposable_loginBtn,userNum)
+
+//번호 미입력시 = '일회용 로그인 번호를 입력하세요.'
+//번호가 틀렸을 시 = '일회용 로그인 번호를 확인한 후 다시 입력해 주세요.'
+disposable_loginBtn.addEventListener('click',()=>{
+    if(userNum.value == ''){
+        disposable_loginBtn_error.innerHTML = '일회용 로그인 번호를 입력하세요.'
+    }else{
+        disposable_loginBtn_error.innerHTML = '일회용 로그인 번호를 확인한 후 다시 입력해 주세요.'
+        userNum.value = ''
+    }
+})
