b75f3713c780d65ddd43f8f08759332f5f900d4b
mina <viva3927@google.com>
2024-01-18 15:23:41 +0900

    DB 마켓컬리

---
+++ shopping_database/script/data.js
@@ -0,0 +1,63 @@
+//상품 DB 저장 위치
+const itemDB = [
+    {
+        itemImg:'./images/plate.jpg',
+        itemTitle:'식빵 플레이트 접시 세트',
+        itemPrice:'14,900원',
+    },
+    {
+        itemImg:'./images/handcream.jpg',
+        itemTitle:'핸드크림 미니 트리오',
+        itemPrice:'39,000원',
+    },
+    {
+        itemImg:'./images/wildChive.jpg',
+        itemTitle:'달래 200G',
+        itemPrice:'5,990원',
+    },
+    {
+        itemImg:'./images/BokChoy.jpg',
+        itemTitle:'청경채 500G',
+        itemPrice:'3,990원',
+    },
+    {
+        itemImg:'./images/mug.jpg',
+        itemTitle:'아이보리앤도트 머그잔',
+        itemPrice:'9,900원',
+    },
+    {
+        itemImg:'./images/denmark.jpg',
+        itemTitle:'덴마크 유산균 이야기',
+        itemPrice:'59,000원',
+    },
+    {
+        itemImg:'./images/ferrero15.jpg',
+        itemTitle:'로쉐 컬렉션 15개입',
+        itemPrice:'15,480원',
+    },
+    {
+        itemImg:'./images/ferrero8.jpg',
+        itemTitle:'로쉐 사각 8개입',
+        itemPrice:'7,480원',
+    },
+    {
+        itemImg:'./images/zinc.jpg',
+        itemTitle:'아연 이뮨케어(30일분',
+        itemPrice:'17,500원',
+    },
+    {
+        itemImg:'./images/kocuten.jpg',
+        itemTitle:'코큐텐 더블케어(30일분)',
+        itemPrice:'31,000원',
+    },
+    {
+        itemImg:'./images/keyring.jpg',
+        itemTitle:'블랙 래빗 키링',
+        itemPrice:'9,900원',
+    },
+    {
+        itemImg:'./images/shampoo.jpg',
+        itemTitle:'피오나 샴푸 400ml',
+        itemPrice:'35,900원',
+    },
+]
